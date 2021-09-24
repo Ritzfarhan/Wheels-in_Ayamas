82d5aade835060efb87e24f82e3b714155888ac9
sorehahhh <78630154+sorehahhh@users.noreply.github.com>
2021-09-25 02:44:27 +0800

    LINK LOGOUT PAGE FOR USER

--- Dashboard/Homepage/assets/js/Homepage.js
+++ Dashboard/Homepage/assets/js/Homepage.js
@@ -29,6 +29,6 @@ const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
     e.preventDefault();
     auth.signOut()
-    window.location.href = "/Homepage/index.html";
+    window.location.href = "/AdminDashboard/pages/pages/authentication/logout.html";
 });
 
