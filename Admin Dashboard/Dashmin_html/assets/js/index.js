3fdc043ac071ee4df1add4b1b24fd4465e3a6505
aunibadrina <88524564+aunibadrina@users.noreply.github.com>
2021-09-26 01:53:49 +0800

    delete unneccessary files

--- Admin Dashboard/Dashmin_html/assets/js/index.js
+++
@@ -1,9 +0,0 @@
-
-
-// logout
-const logout = document.querySelector('#logout');
-logout.addEventListener('click', (e) => {
-    e.preventDefault();
-    auth.signOut()
-    window.alert("You are Logged out");
-});
