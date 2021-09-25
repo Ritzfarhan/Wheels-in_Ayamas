80fab9a3c5860816a029e2e4653edc216974492a
Ritzfarhan <ritzfarhan@gmail.com>
2021-09-25 22:38:47 +0800

    log out

--- Dashboard/AdminDashboard/assets/js/index.js
+++ Dashboard/AdminDashboard/assets/js/index.js
@@ -17,7 +17,27 @@ logout.addEventListener('click', (e) => {
     e.preventDefault();
     auth.signOut()
     //window.alert("You are Logged out");
-    Swal.fire({ title: "Logged out", text: " Redirecting to homepage", type: "warning", confirmButtonClass: "btn long", buttonsStyling: !1 });
+    //Swal.fire({ title: "Logged out", text: " Redirecting to homepage", type: "warning", confirmButtonClass: "btn long", buttonsStyling: !1 });
+
+    var t;
+    Swal.fire({
+        title: "Logged out",
+        html: "Redirecting to Homepage in <strong></strong> seconds.",
+        timer: 2000,
+        confirmButtonClass: "btn long",
+        buttonsStyling: !1,
+        onBeforeOpen: function () {
+            Swal.showLoading(),
+                (t = setInterval(function () {
+                    Swal.getContent().querySelector("strong").textContent = Swal.getTimerLeft();
+                }, 100));
+        },
+        onClose: function () {
+            clearInterval(t);
+        },
+    }).then(function (t) {
+        t.dismiss === Swal.DismissReason.timer && console.log("I was closed by the timer");
+    });
     setTimeout(function () {
         window.location.href = "/Homepage/index.html";
     }, 2000);
