efd773ff3f40d1aa526f3db6ce17944577335cef
sorehahhh <78630154+sorehahhh@users.noreply.github.com>
2021-10-07 21:03:51 +0800

    edit profile

---
+++ Dashboard/AdminDashboard/assets/js/edit-profile.js
@@ -0,0 +1,22 @@
+       //===== Ready Data ===
+      // const Username = document.querySelector('.user-name-edit');
+      // const f_name = document.querySelector('.f_name-edit');
+      // const l_name = document.querySelector('.l_name-edit');       
+
+       //function Ready(){
+       //     Username = document.getElementById('user-name-edit').value;
+        //    email = document.getElementById("email-edit").value;
+        //    f_name = document.getElementById('f_name-edit').value;
+        //    l_name = document.getElementById('l_name-edit').value;
+      //  }
+        
+        //===== Select data ===
+       // document.getElementById('update').onclick = function(){
+        //    Ready();
+        //   firebase.database().ref('users/'+email).update({
+          //      UserName: Username,
+        //        FirstName: f_name,
+        //        LastName : l_name
+       // });
+       // }
+
