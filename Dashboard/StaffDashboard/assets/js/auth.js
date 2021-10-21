670cc68410a4d8b0aae11581db507f1d40843b3b
Ritzfarhan <ritzfarhan@gmail.com>
2021-10-22 06:50:32 +0800

    Update auth.js

--- Dashboard/StaffDashboard/assets/js/auth.js
+++ Dashboard/StaffDashboard/assets/js/auth.js
@@ -44,6 +44,29 @@ auth.onAuthStateChanged(user => {
   logout.addEventListener('click', (e) => {
     e.preventDefault();
     auth.signOut()
+
+    var t;
+    Swal.fire({
+        title: "Logged out",
+        html: "Redirecting to Homepage in <strong></strong> seconds.",
+        timer: 2000,
+        confirmButtonClass: "btn long",
+        buttonsStyling: !1,
+        onBeforeOpen: function () {
+            Swal.showLoading(),
+                (t = setInterval(function () {
+                    Swal.getContent().querySelector("strong").textContent = Swal.getTimerLeft();
+                }, 100));
+        },
+        onClose: function () {
+            clearInterval(t);
+        },
+    }).then(function (t) {
+        t.dismiss === Swal.DismissReason.timer && console.log("I was closed by the timer");
+    });
+    setTimeout(function () {
+        window.location.href = "/Homepage/index.html";
+    }, 2000);
   });
   
   
