104b502defb5f59d6ea1f688c9424c39391ec476
Ritzfarhan <ritzfarhan@gmail.com>
2021-10-22 05:55:02 +0800

    cart number

---
+++ Dashboard/Homepage/assets/js/cartnumber.js
@@ -0,0 +1,17 @@
+function getCartItems() {
+    db.collection("cart-items").onSnapshot((snapshot) => {
+        let totalCount = 0;
+        snapshot.docs.forEach((doc)=>{
+            totalCount += doc.data().quantity;
+        })
+        console.log(totalCount);
+        setCartCounter(totalCount);
+    })
+}
+
+function setCartCounter(totalCount) {
+    // cart-item-number
+    document.querySelector(".count").innerText = totalCount;
+}
+
+getCartItems();
